e0880dd72755158c4e18452e0cd99ce9d9d020f4
dehkai <ydk1421@gmail.com>
2023-11-05 22:39:58 +0800

    Home Page

---
+++ src/Home.js
@@ -0,0 +1,16 @@
+import { useState, useEffect } from "react";
+import BlogList from "./BlogList";
+import useFetch from "./useFetch";
+const Home = () => {
+    const{data, isPending, error} = useFetch('http://localhost:8000/blogs')
+
+    return (
+        <div className="home">
+            {error && <div>{error}</div>}
+            {isPending && <div>Loading...</div>}
+            {data && <BlogList blogs = {data} title = "All Blogs"></BlogList>}
+        </div>
+     );
+}
+ 
+export default Home;
