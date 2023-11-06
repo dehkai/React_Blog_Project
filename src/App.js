21c8e2690237e1d8595161d1dd57f26aad88b884
dehkai <ydk1421@gmail.com>
2023-11-06 22:39:56 +0800

    All features
    Show blog details, create new blogs, delete blogs and 404 error page

--- src/App.js
+++ src/App.js
@@ -1,18 +1,34 @@
 import logo from './logo.svg';
 import Navbar from './Navbar';
 import Home from './Home';
-
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import Create from "./Create";
+import BlogDetails from "./BlogDetails";
+import NotFound from "./NotFound";
 function App() {
 
-  const title = "Welcome to the new blog";
-  const likes = 50;
-  return (
-    <div className="App">
-      <Navbar></Navbar>
-      <div className="content">
-        <Home></Home>
-      </div>
-    </div>
+    return (
+      <Router>
+        <div className="App">
+          <Navbar></Navbar>
+          <div className="content">
+            <Switch>
+                <Route exact path = "/">
+                    <Home></Home>
+                </Route>
+                <Route path = "/create">
+                    <Create />
+                </Route>
+                <Route path = "/blogs/:id">
+                    <BlogDetails />
+                </Route>
+                <Route path = "*">
+                 <NotFound />
+                </Route>
+            </Switch>
+          </div>
+        </div>
+      </Router>
   );
 }
 
--- src/BlogList.js
+++ src/BlogList.js
@@ -1,3 +1,5 @@
+import {Link} from "react-router-dom";
+
 const BlogList = ({blogs, title, handleDelete}) => {
     // const blogs = props.blogs;
     // const title = props. title;
@@ -6,8 +8,10 @@ const BlogList = ({blogs, title, handleDelete}) => {
             <h2>{title}</h2>
             {blogs.map((blog)=>(
             <div className="blog-preview" key={blog.id}>
-                <h2>{blog.title}</h2>
-                <p>Written by {blog.author}</p>
+                <Link to = {`/blogs/${blog.id}`}>
+                    <h2>{blog.title}</h2>
+                    <p>Written by {blog.author}</p>
+                </Link>
             </div>
            ))}</div>
     );
